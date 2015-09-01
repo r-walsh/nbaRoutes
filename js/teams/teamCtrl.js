eb3a0cad7506cbb39898838b60013b8912612e91
Ryan Walsh <ryanwalsh@Ryans-MacBook-Pro.local>
2015-09-01 12:20:50 -0600

    Most of the heavy lifting complete, just need to finish display function

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,27 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+
+	$scope.teamData = teamData;
+	$scope.newGame = {};
+	$scope.showNewGameForm = false;
+
+	$scope.toggleNewGameForm = function() {
+		!$scope.showNewGameForm;
+	}
+
+	if ($routeParams.team === 'utahjazz') {
+		$scope.homeTeam = 'Utah Jazz';
+	} else if ($routeParams.team === 'losangeleslakers') {
+		$scope.homeTeam = 'Los Angeles Lakers';
+	} else if ($routeParams.team === 'miamiheat') {
+		$scope.homeTeam = 'Miami Heat';
+	}
+
+	$scope.submitGame = function() {
+		$scope.newGame.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();
+		$scope.newGame = teamService.addNewGame
+	}
+
 
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,40 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+	this.addNewGame = function(gameObj) {
+		var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+
+		if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+			gameObj.won = true;
+		} else {
+			gameObj.won = false;
+		}
+
+		return $http.post(url, gameObj);
+	}
+
+	this.getTeamData = function(team) {
+		var deferred = $q.defer(),
+			url = 'https://api.parse.com/1/classes/' + team;
+
+		$http.get(url)
+			.then(function(data) {
+				var results = data.data.results,
+					wins = 0,
+					losses = 0;
+				for (var i = 0; i < results.length; i++) {
+					if (results[i].won = true) {
+						wins++;
+					} else {
+						losses++;
+					}
+				}
+				results.wins = wins;
+				results.losses = losses;
+				deferred.resolve(results);
+			});
+			return deferred.promise
+	}
+
+
 });
